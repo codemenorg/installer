225c4699eb2730fd13fcbc7cad04fce12ea4eeae
Ibrahim Al Naz Rana <ranabd36@gmail.com>
2019-11-11 15:33:26 +0600

    Update route

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,9 +5,13 @@ Route::prefix('install')
     ->namespace('Codemen\Installer\Controllers')
     ->middleware(['web', 'install'])
     ->group(function () {
-        Route::get('{types}', 'ViewController')->name('types');
+        Route::get('{types}', 'ViewController')
+            ->name('types')
+            ->where(['types' => implode('|', array_keys(config('installer.routes', [])))]);
 
-        Route::post('setup/{types}', 'StoreController')->name('types.store');
+        Route::post('setup/{types}', 'StoreController')
+            ->name('types.store')
+            ->where(['types' => implode('|', array_keys(config('installer.routes', [])))]);
 
         Route::get('/', [
             'as' => 'index',
